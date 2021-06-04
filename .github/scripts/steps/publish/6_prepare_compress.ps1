e569894d9d180446e0970bc1b7e4b3259a469a90
JyCyunMe <JyCyun@JyCyun.me>
2021-06-05 03:12:37 +0800

    fix: notification test

--- .github/scripts/steps/publish/6_prepare_compress.ps1
+++ .github/scripts/steps/publish/6_prepare_compress.ps1
@@ -21,7 +21,6 @@ foreach ($file in $filesX86) {
     cp $file $PUBLISH_PATH_X86\
 }
 
-
 mkdir -p ($RELEASE_PATH="${env:PUBLISH_PATH}\releases")
 echo "RELEASE_PATH=$RELEASE_PATH" >> $env:GITHUB_ENV
 $RELEASE_PKG_X64="$RELEASE_PATH\Clash.Mini_${env:GIT_TAG}_x64.7z"
@@ -29,5 +28,9 @@ $RELEASE_PKG_X86="$RELEASE_PATH\Clash.Mini_${env:GIT_TAG}_x86.7z"
 echo "RELEASE_PKG_X64=$RELEASE_PKG_X64" >> $env:GITHUB_ENV
 echo "RELEASE_PKG_X86=$RELEASE_PKG_X86" >> $env:GITHUB_ENV
 
-echo ($UNIX_RELEASE_PATH=($RELEASE_PATH -replace '([A-Z]):\\', '/$1/').Replace("\", "/"))
-echo "::set-output name=unix-release-path::$UNIX_RELEASE_PATH"
+# $RELEASE_PKG_X64=${RELEASE_PKG_X64##*\\}
+# $RELEASE_PKG_X86=${RELEASE_PKG_X86##*\\}
+$RELEASE_PKG_X64=$RELEASE_PKG_X64.Substring($RELEASE_PKG_X64.LastIndexOf("\")+1)
+$RELEASE_PKG_X86=$RELEASE_PKG_X86.Substring($RELEASE_PKG_X86.LastIndexOf("\")+1)
+echo "::set-output name=release-pkg-x64::$RELEASE_PKG_X64"
+echo "::set-output name=release-pkg-x86::$RELEASE_PKG_X86"
