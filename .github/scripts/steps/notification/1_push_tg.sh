7a6e6e11fe87e4c26e043034cdbba889ebcb30bf
JyCyunMe <JyCyun@JyCyun.me>
2021-06-05 01:32:43 +0800

    fix: notification test

--- .github/scripts/steps/notification/1_push_tg.sh
+++ .github/scripts/steps/notification/1_push_tg.sh
@@ -1,12 +1,13 @@
 # 推送到TG
 
-pip install requests BeautifulSoup4
-touch ./.github/output/release_log
-python3 ./.github/scripts/steps/notification/release_text.py || {
-  echo -e "\n run release_text.py failed."
-  exit 1
-}
-RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' ./.github/output/release_log)" --form-string parse_mode="HTML" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"GitHub\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini\"}]]}")
+#pip install requests BeautifulSoup4
+#touch ./.github/output/release_log
+#python3 ./.github/scripts/steps/notification/release_text.py || {
+#  echo -e "\n run release_text.py failed."
+#  exit 1
+#}
+#RLT=$(curl --location --request POST https://api.telegram.org/bot${{ seN  }}/sendMessage -s --form-string chat_id=${{t-id }} --form-string text="$(perl -lne 'print;' ./RELEASELOG.md)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini/releases/tag/${{ needs.build-release-windows.outputs.git-tag }}\"},{\"text\":\"GitHub\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini\"}]]}")
+RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' ./RELEASELOG.md)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"GitHub\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini\"}]]}")
 #RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' ./RELEASELOG.md)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"GitHub\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini\"}]]}")
 
 if [[ -e $(echo $RLT | jq ".ok") ]]; then
