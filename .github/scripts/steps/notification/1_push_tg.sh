63f64432913fdc004bc4a1200e548d83fd78be0e
JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>
2021-06-05 16:06:46 +0800

    fix: notification test
    Signed-off-by: JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>

--- .github/scripts/steps/notification/1_push_tg.sh
+++ .github/scripts/steps/notification/1_push_tg.sh
@@ -1,12 +1,11 @@
 # 推送到TG
 
-##pip install requests BeautifulSoup4
-##touch ./.github/output/release_log
-##python3 ./.github/scripts/steps/notification/release_text.py || {
-##  echo -e "\n run release_text.py failed."
-##  exit 1
-##}
-RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' ./RELEASELOG.md)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"GitHub\",\"url\":\"https://github.com/JyCyunMe/Clash.Mini\"}]]}")
+if [ -f "./RELEASELOG_NOTIFY.md" ]; then
+  RELEASE_LOG="./RELEASELOG_NOTIFY.md"
+else
+  RELEASE_LOG="./RELEASELOG.md"
+fi
+RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMessage -s --form-string chat_id=${CHAT_ID} --form-string text="$(perl -lne 'print;' $RELEASE_LOG)" --form-string parse_mode="Markdown" --form-string disable_web_page_preview="true" --form-string allow_sending_without_reply="true" --form-string reply_markup="{\"inline_keyboard\":[[{\"text\":\"Download\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini/releases/tag/${GIT_TAG}\"},{\"text\":\"Star\",\"url\":\"https://github.com/Clash-Mini/Clash.Mini\"}]]}")
 IS_OK=$(echo $RLT | jq ".ok")
 if [ $IS_OK ]; then
   MSG_ID=$(echo $RLT | jq ".result.message_id")
@@ -14,7 +13,7 @@ if [ $IS_OK ]; then
   PART_X64=$(echo "${DT_STR}_Clash.Mini_X64" | base64 | tr -s "=" 2)
   PART_X86=$(echo "${DT_STR}_Clash.Mini_X86" | base64 | tr -s "=" 2)
 
-  RELEASE_URL="https://github.com/JyCyunMe/Clash.Mini/releases/download/${GIT_TAG}"
+  RELEASE_URL="https://github.com/Clash-Mini/Clash.Mini/releases/download/${GIT_TAG}"
   echo $RELEASE_URL
   RELEASE_PATH="$(pwd)/releases"
   mkdir -p $RELEASE_PATH
@@ -30,7 +29,7 @@ if [ $IS_OK ]; then
 
   RELEASE_SHA256_X64=$(cat "${RELEASE_PATH}/${RELEASE_PKG_X64}.sha256" | tr -d "\n")
   RELEASE_SHA256_X86=$(cat "${RELEASE_PATH}/${RELEASE_PKG_X86}.sha256" | tr -d "\n")
-  RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMediaGroup -s --form-string chat_id=${CHAT_ID} --form-string reply_to_message_id=$MSG_ID --form $PART_X64=@"${RELEASE_PATH}/${RELEASE_PKG_X64}" --form $PART_X86=@"${RELEASE_PATH}/${RELEASE_PKG_X86}" --form-string media="[{\"type\": \"document\",\"media\": \"attach://$PART_X64\",\"caption\": \"SHA256: ${RELEASE_SHA256_X64}\",\"parse_mode\": \"Markdown\"},{\"type\": \"document\",\"media\": \"attach://$PART_X86\",\"caption\": \"SHA256: ${RELEASE_SHA256_X64}\",\"parse_mode\": \"Markdown\"}]")
+  RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_TOKEN}/sendMediaGroup -s --form-string chat_id=${CHAT_ID} --form-string reply_to_message_id=$MSG_ID --form $PART_X64=@"${RELEASE_PATH}/${RELEASE_PKG_X64}" --form $PART_X86=@"${RELEASE_PATH}/${RELEASE_PKG_X86}" --form-string media="[{\"type\": \"document\",\"media\": \"attach://$PART_X64\",\"caption\": \"SHA256: ${RELEASE_SHA256_X64}\",\"parse_mode\": \"Markdown\"},{\"type\": \"document\",\"media\": \"attach://$PART_X86\",\"caption\": \"SHA256: ${RELEASE_SHA256_X86}\",\"parse_mode\": \"Markdown\"}]")
   IS_OK=$(echo $RLT | jq ".ok")
   if [ ! $IS_OK ]; then
     echo "Reply files to channel failed. Response: "
--- RELEASELOG_NOTIFY.md
+++ RELEASELOG_NOTIFY.md
@@ -1,33 +1,31 @@
-## Release Log
+*Release Log*
 
-### `v0.1.3-pre`
+`v0.1.3-pre`
 
-Release on _**2021-06-05 16:00**_
+Release on _*2021-06-05 16:00*_
 
-**Features**
+*Features*
 
 1. Supported switch proxy on tray menu ([#8](https://github.com/Clash-Mini/Clash.Mini/issues/8))
 2. Optimized some performance
 
-**Fixes**
+*Fixes*
 
 1. Fixed some problems about Tasksch
 2. Set the default value when the Regedit value cannot be obtained
 
----
+*发行日志*
 
-## 发行日志
+`v0.1.3-pre`
 
-### `v0.1.3-pre`
+发布于 _*2021-06-05 16:00*_
 
-发布于 _**2021-06-05 16:00**_
-
-**特性**
+*特性*
 
 1. 支持在托盘菜单切换代理 ([#8](https://github.com/Clash-Mini/Clash.Mini/issues/8))
 2. 优化了一些性能
 
-**修复**
+*修复*
 
 1. 修复任务计划的问题
 2. 无法读取到注册表值时自动设置默认值
