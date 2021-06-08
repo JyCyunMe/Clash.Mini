8d9cb96ffdf9ece288c4749eec8be8b516d3aa45
JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>
2021-06-08 18:25:54 +0800

    fix: notification test
    Signed-off-by: JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>

--- .github/scripts/steps/build/4_prepare_upload_dev.ps1
+++ .github/scripts/steps/build/4_prepare_upload_dev.ps1
@@ -6,7 +6,9 @@ $BUILD_X64_FILENAME = "Clash.Mini_${env:GIT_BRANCH}_v${env:BUILD_VERSION}_${env:
 $BUILD_X86_FILENAME = "Clash.Mini_${env:GIT_BRANCH}_v${env:BUILD_VERSION}_${env:SHORT_SHA}_x86.exe"
 echo "BUILD_X64_FILENAME=$BUILD_X64_FILENAME" >> $env:GITHUB_ENV
 echo "BUILD_X86_FILENAME=$BUILD_X86_FILENAME" >> $env:GITHUB_ENV
-cp .\Clash.Mini_dev_*.exe $env:PUBLISH_PATH\
+cp .\Clash.Mini_dev_x64.exe $env:PUBLISH_PATH\$BUILD_X64_FILENAME
+cp .\Clash.Mini_dev_x86.exe $env:PUBLISH_PATH\$BUILD_X86_FILENAME
+
 echo "Ready to upload the following file(s):"
 ls $env:PUBLISH_PATH
 
