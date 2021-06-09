3fd3a18164618760df8fa766f58aeac518270df8
JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>
2021-06-09 10:58:20 +0800

    fix: notification test
    Signed-off-by: JyCyunMe <45283199+JyCyunMe@users.noreply.github.com>

--- .github/scripts/steps/notification/2_push_files_tg_dev.sh
+++ .github/scripts/steps/notification/2_push_files_tg_dev.sh
@@ -6,14 +6,13 @@ PART_X86=$(echo "${DT_STR}_Clash.Mini_X86_${GITHUB_SHA}" | base64 | tr -s "=" 2)
 
 RUNNER_URL="https://github.com/JyCyunMe/Clash.Mini/actions/runs/${GITHUB_RUN_ID}"
 echo "$RUNNER_URL"
-ls -lah "$ARTIFACTS_PATH"
 ls -lahR "$ARTIFACTS_PATH"
-ls "${ARTIFACTS_PATH}/${BUILD_X64_FILENAME}/${BUILD_X64_FILENAME}"
-ls "${ARTIFACTS_PATH}/${BUILD_X86_FILENAME}/${BUILD_X86_FILENAME}"
+ARTIFACT_X64="${ARTIFACTS_PATH}/${BUILD_X64_FILENAME}"
+ARTIFACT_X86="${ARTIFACTS_PATH}/${BUILD_X86_FILENAME}"
 
-ARTIFACT_X64_SHA256=$(cat "${ARTIFACTS_PATH}/${BUILD_X64_FILENAME}.sha256/${BUILD_X64_FILENAME}.sha256" | tr -d "\n")
-ARTIFACT_X86_SHA256=$(cat "${ARTIFACTS_PATH}/${BUILD_X64_FILENAME}.sha256/${BUILD_X64_FILENAME}.sha256" | tr -d "\n")
-RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMediaGroup -s --form-string chat_id=${UPLOAD_CHAT_ID} --form-string media="[{\"type\": \"document\",\"media\": \"attach://$PART_X64\",\"caption\": \"SHA256: ${ARTIFACT_X64_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"},{\"type\": \"document\",\"media\": \"attach://$PART_X86\",\"caption\": \"SHA256: ${ARTIFACT_X86_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"}]")
+ARTIFACT_X64_SHA256=$(cat "${ARTIFACT_X64}.sha256/${BUILD_X64_FILENAME}.sha256" | tr -d "\n")
+ARTIFACT_X86_SHA256=$(cat "${ARTIFACT_X86}.sha256/${BUILD_X64_FILENAME}.sha256" | tr -d "\n")
+RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMediaGroup -s --form-string chat_id=${UPLOAD_CHAT_ID} --form $PART_X64=@"${ARTIFACT_X64}/${BUILD_X64_FILENAME}" --form $PART_X86=@"${ARTIFACT_X86}/${BUILD_X86_FILENAME}" --form-string media="[{\"type\": \"document\",\"media\": \"attach://$PART_X64\",\"caption\": \"SHA256: ${ARTIFACT_X64_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"},{\"type\": \"document\",\"media\": \"attach://$PART_X86\",\"caption\": \"SHA256: ${ARTIFACT_X86_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"}]")
 #RLT=$(curl --location --request POST https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMediaGroup -s --form-string chat_id=${UPLOAD_CHAT_ID} --form $PART_X64=@"${ARTIFACTS_PATH}/${BUILD_X64_FILENAME}/${BUILD_X64_FILENAME}" --form $PART_X86=@"${ARTIFACTS_PATH}/${BUILD_X86_FILENAME}/${BUILD_X86_FILENAME}" --form-string media="[{\"type\": \"document\",\"media\": \"attach://$PART_X64\",\"caption\": \"SHA256: ${ARTIFACT_X64_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"},{\"type\": \"document\",\"media\": \"attach://$PART_X86\",\"caption\": \"SHA256: ${ARTIFACT_X86_SHA256}\n\n_[${GITHUB_WORKFLOW} #${GITHUB_RUN_NUMBER}](${RUNNER_URL})_\",\"parse_mode\": \"Markdown\"}]")
 IS_OK=$(echo $RLT | jq ".ok")
 echo $RLT | jq .
